1877009eaa097b1ae918f4c3e80facd6036a989e
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-07-07 12:53:57 +0300

    Added image link && firebase deploy

--- src/Header.js
+++ src/Header.js
@@ -3,24 +3,31 @@ import SearchIcon from "@material-ui/icons/Search"
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
 import { Link } from "react-router-dom"
 import {useStateValue} from './StateProvider';
+import {auth} from './firebase'
 
-function Header() {
-    const [{basket}, dispatch]  = useStateValue();
+function Header() { 
+    const [{basket,user}, dispatch]  = useStateValue();
+
+    const handleAuthentication =()=>{
+        if(user){
+            auth.signOut();
+        }
+    }
 
     return (
         <div className="header">
             <Link to="/">
-                <img className="header_logo" src={"amazon.png"} alt="" />
+                <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
             </Link>
             <div className="header_search">
                 <input type="text" className="header_searchInput" />
                 <SearchIcon className="header_searchIcon" />
             </div>
             <div className="header_nav">
-                <Link to="./login">
-                <div className="header_option">
+                <Link to={!user && "/login"}>
+                <div onClick={handleAuthentication} className="header_option">
                     <span className="header_optionLineOne">Hello Guest</span>
-                    <span className="header_optionLineTwo">Sign In</span>
+                    <span className="header_optionLineTwo">{user ? "Sign Out" :"Sign In"}</span>
                 </div>
                 </Link>
                 <div className="header_option">
